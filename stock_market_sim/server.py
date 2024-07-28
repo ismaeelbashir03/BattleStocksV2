65ba8a01c18c0b02c3cc9f2f1f3219c2216d3499
ismaeelbashir03 <ismaeelbashir2003@gmail.com>
2024-07-28 18:11:56 +0100

    changed endpoint responses and bodies. Added swagger docs to endpoints. Updated tests

--- stock_market_sim/server.py
+++ stock_market_sim/server.py
@@ -1,167 +1,270 @@
+import string
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask_restx import fields, Resource, Api
 import random
 import threading
 import time
 import uuid
 
-"""
+
+'''
 APP INIT
-"""
+'''
 app = Flask(__name__)
 api = Api(app)
 
-"""
+
+'''
 GLOBAL VARS
-"""
+'''
 exchanges = {}
 TICKS_PER_SECOND = 1
+CODE_LENGTH = 6
+DIFFICULTY_MAP = {
+    1: {
+        'stock_std': 0.5,
+        'headline_min_impact': 3,
+        'headline_max_impact': 3
+    },
+    2: {
+        'stock_std': 0.65,
+        'headline_min_impact': 2,
+        'headline_max_impact': 3,
+    },
+    3: {
+        'stock_std': 0.8,
+        'headline_min_impact': 1,
+        'headline_max_impact': 3,
+    },
+    4: {
+        'stock_std': 1,
+        'headline_min_impact': 0.95,
+        'headline_max_impact': 2,
+    },
+    5: {
+        'stock_std': 2,
+        'headline_min_impact': 0.9,
+        'headline_max_impact': 1.5,
+    }
+}
+STARTING_PRICE_RANGE = range(50, 150)
+STARTING_CASH = 1000
 
 
-"""
-ENDPOINTS - TODO: add swagger generation with models and flask-restplus
-"""
+'''
+MODELS
+'''
+config_model = api.model('ConfigBody', {
+    'stocks': fields.List(fields.String, required=True, description='List of stocks to include in the simulation.'),
+    'duration': fields.Integer(required=True, description='Duration of the simulation in minutes.'),
+    'difficulty': fields.Integer(required=True, description='Difficulty level of the simulation (1 to 5 inclusive).'),
+})
+
+news_model = api.model('NewsBody', {
+    'stock': fields.String(required=True, description='Stock to affect.'),
+    'impact': fields.String(required=True, description='Sentiment of the news headline (up or down).'),
+})
+
+order_model = api.model('Order', {
+    'userId': fields.String,
+    'stock': fields.String,
+    'quantity': fields.Integer,
+    'type': fields.String
+    })
+
+connect_model = api.model('ConnectBody', {
+    'name': fields.String(required=True, description='Name of the user connecting to the exchange.'),
+})
+
+
+'''
+HOST ENDPOINTS
+'''
+@api.route('/start-server', methods=['POST'])
+@api.expect(config_model)
+@api.response(200, 'Success')
 class Config(Resource):
     def post(self):
-        """
-        POST request to set the configuration and start the market simulation for a specific exchange.
-
-        Request body:
-            - stocks: dict content: {'stock_name': {'price': float}}
-            - users: dict content: ('user_id': {'assets': {'stock_name': int}, 'money': float})
-            - ticks_per_news: int
-            - stock_std: float
-            - headline_min_impact: float
-            - headline_max_impact: float
-        """
         global exchanges
         
         config_data = request.json
-        exchange_id = str(uuid.uuid4())
+        duration = config_data['duration']
+        settings = DIFFICULTY_MAP[config_data['difficulty']]
+        stocks = {stock: random.choice(STARTING_PRICE_RANGE) for stock in config_data['stocks']}
+        
+        exchange_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(CODE_LENGTH))
+
+        while exchange_id in exchanges:
+            exchange_id = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(CODE_LENGTH))
 
         if exchange_id not in exchanges:
             exchanges[exchange_id] = {
-                'config': {},
+                'settings': {},
+                'stocks': {},
+                'news_headlines': [],
+                'users': {},
                 'tick_count': 0,
                 'STARTED': False,
                 'lock': threading.Lock()
             }
         
         with exchanges[exchange_id]['lock']:
-            exchanges[exchange_id]['config'].update(config_data)
+            exchanges[exchange_id]['settings'].update(settings)
+            exchanges[exchange_id]['stocks'].update(stocks)
             exchanges[exchange_id]['tick_count'] = 0
             exchanges[exchange_id]['STARTED'] = True
         
-        start_simulation_thread(exchange_id)
+        start_simulation_thread(exchange_id, duration)
 
-        return jsonify({"exchange_id": exchange_id, "message": f"Configuration updated and market simulation started for exchange {exchange_id}."})
+        response = jsonify({'exchange_id': exchange_id, 'message': f'Configuration updated and market simulation started for exchange {exchange_id}.'})
+        response.status_code = 200
+        return response
 
+@api.route('/<string:exchange_id>/market-data', methods=['GET'])
+@api.response(200, 'Success')
+@api.response(400, 'Validation Error')
 class MarketData(Resource):
     def get(self, exchange_id):
-        """
-        GET request to get the current market data for a specific exchange.
-
-        Query parameters:
-            - exchange_id: str
-
-        Response:
-            - stocks: dict
-        """
         global exchanges
         exchange_id = str(exchange_id)
 
         if exchange_id not in exchanges:
-            return jsonify({"message": "Exchange not found."})
+            response = jsonify({'message': 'Exchange not found.'})
+            response.status_code = 400
+            return response
 
         with exchanges[exchange_id]['lock']:
             if not exchanges[exchange_id]['STARTED']:
-                return jsonify({"message": "Market simulation not started."})
-            return jsonify(exchanges[exchange_id]['config']['stocks'])
+                response = jsonify({'message': 'Market simulation not started.'})
+                response.status_code = 400
+                return response
+            
+            account_details = [details for details in exchanges[exchange_id]['users'].values()]
+            prices = exchanges[exchange_id]['stocks']
 
+            response = jsonify({'details': account_details, 'prices': prices})
+            response.status_code = 200
+            return response
+
+@api.route('/<string:exchange_id>/add-news', methods=['POST'])
+@api.expect(news_model)
+@api.response(200, 'Success')
+@api.response(400, 'Validation Error')
+class News(Resource):
+    def post(self, exchange_id):
+        global exchanges
+        exchange_id = str(exchange_id)
+
+        if exchange_id not in exchanges:
+            response = jsonify({'message': 'Exchange not found.'})
+            response.status_code = 400
+            return response
+
+        headline = {'stock': request.json['stock'], 'sentiment': request.json['impact']}
+
+        with exchanges[exchange_id]['lock']:
+            exchanges[exchange_id]['news_headlines'].append(headline)
+
+        response = jsonify({'message': f'News headline published for exchange {exchange_id}. Market will be affected.'})
+        response.status_code = 200
+        return response
+
+
+'''
+CLIENT ENDPOINTS
+'''
+@api.route('/<string:exchange_id>/connect', methods=['POST'])
+@api.expect(connect_model)
+@api.response(200, 'Success')
+@api.response(400, 'Validation Error')
+class Connect(Resource):
+    def post(self, exchange_id):
+        global exchanges
+
+        name = request.json['name']
+        userId = str(uuid.uuid4())
+        exchanges[exchange_id]['users'].update({userId: {'name': name, 'cash': STARTING_CASH, 'assets': {}, 'value': STARTING_CASH}})
+
+        if exchange_id not in exchanges:
+            response = jsonify({'message': 'Exchange not found.'})
+            response.status_code = 400
+            return response
+
+        response = jsonify({'userId': userId})
+        response.status_code = 200
+        return response
+
+@api.route('/<string:exchange_id>/order', methods=['POST'])
+@api.expect(order_model)
+@api.response(200, 'Success')
+@api.response(400, 'Validation Error')
 class Orders(Resource):
-    def post(self, exchange_id, user_id):
-        """
-        POST request to place an order for a specific exchange.
-
-        Query parameters:
-            - exchange_id: str
-            - user_id: str
-
-        Request body:
-            - stock: str
-            - quantity: int
-            - type: str (buy/sell)
-        """
+    def post(self, exchange_id):
         global exchanges
+
         exchange_id = str(exchange_id)
-        
         order_data = request.json
+        user_id = order_data['userId']
 
         if exchange_id not in exchanges:
-            return jsonify({"message": "Exchange not found."})
+            response = jsonify({'message': 'Exchange not found.'})
+            response.status_code = 400
+            return response
 
         with exchanges[exchange_id]['lock']:
             if not exchanges[exchange_id]['STARTED']:
-                return jsonify({"message": "Market simulation not started."})
-
-            config = exchanges[exchange_id]['config']
+                response = jsonify({'message': 'Market simulation not started.'})
+                response.status_code = 400
+                return response
             
-            if user_id not in config['users']:
-                return jsonify({"message": "User not found."})
+            if user_id not in exchanges[exchange_id]['users']:
+                response = jsonify({'message': 'User not found.'})
+                response.status_code = 400
+                return response
             
-            user = config['users'][user_id]
+            user = exchanges[exchange_id]['users'][user_id]
             stock = order_data['stock']
             quantity = order_data['quantity']
-            price = config['stocks'][stock]['price']
+            price = exchanges[exchange_id]['stocks'][stock]
 
-            if order_data['type'] == 'buy' and user['money'] >= quantity * price:
-                user['money'] -= quantity * price
+            if order_data['type'] == 'buy' and user['cash'] >= quantity * price:
+                user['cash'] -= quantity * price
                 user['assets'][stock] = user['assets'].get(stock, 0) + quantity
             
             elif order_data['type'] == 'sell' and user['assets'].get(stock, 0) >= quantity:
-                user['money'] += quantity * price
+                user['cash'] += quantity * price
                 user['assets'][stock] -= quantity
             
-            config['users'][user_id] = user
+            exchanges[exchange_id]['users'][user_id] = user
 
-        return jsonify(user)
+        response = jsonify({'message': f'Order executed: {order_data["type"]} {quantity} {stock} for {price}.'})
+        response.status_code = 200
+        return response
 
-class News(Resource):
-    def post(self, exchange_id):
-        """
-        POST request to set the news headlines for a specific exchange.
 
-        Query parameters:
-            - exchange_id: str
-
-        Request body:
-            - headline: dict
-        """
-        global exchanges
-        exchange_id = str(exchange_id)
-
-        if exchange_id not in exchanges:
-            return jsonify({"message": "Exchange not found."})
-
-        headline = request.json["headline"]
-
-        with exchanges[exchange_id]['lock']:
-            exchanges[exchange_id]['config']['news_headlines'].append(headline)
-
-        return jsonify({"message": f"News headline published for exchange {exchange_id}. Market will be affected."})
-
+'''
+ROUTES
+'''
 api.add_resource(News, '/<string:exchange_id>/add-news')
-api.add_resource(Config, '/<string:exchange_id>/start-server')
+api.add_resource(Config, '/start-server')
 api.add_resource(MarketData, '/<string:exchange_id>/market-data')
-api.add_resource(Orders, '/<string:exchange_id>/order/<string:user_id>')
+api.add_resource(Connect, '/<string:exchange_id>/connect')
+api.add_resource(Orders, '/<string:exchange_id>/order')
 
-"""
+
+'''
 SIMULATION
-"""
-def simulate_market(exchange_id):
+'''
+def simulate_market(exchange_id, timeout):
     global exchanges
-    
+
+    start = time.time()
     while True:
+        if time.time() - start > timeout * 60:
+            with exchanges[exchange_id]['lock']:
+                exchanges[exchange_id]['STARTED'] = False
+            break
+
         time.sleep(TICKS_PER_SECOND)
 
         with exchanges[exchange_id]['lock']:
@@ -169,20 +272,23 @@ def simulate_market(exchange_id):
                 continue
 
             exchanges[exchange_id]['tick_count'] += 1
-            config = exchanges[exchange_id]['config']
+            config = exchanges[exchange_id]
 
             for stock in config['stocks']:
-                config['stocks'][stock]['price'] += random.gauss(0, config['stock_std'])
+                config['stocks'][stock] += random.gauss(0, config['settings']['stock_std'])
             
             if config['news_headlines']:
                 headline = config['news_headlines'].pop(0)
                 stock = headline['stock']
                 sentiment = headline['sentiment']
-                impact = random.uniform(config['headline_min_impact'], config['headline_max_impact'])
-                config['stocks'][stock]['price'] *= impact if sentiment == 'up' else 1/impact
+                impact = random.uniform(config['settings']['headline_min_impact'], config['settings']['headline_max_impact'])
+                config['stocks'][stock] *= impact if sentiment == 'up' else 1/impact
+        
+    # delete exchange if simulation is over
+    del exchanges[exchange_id]
 
-def start_simulation_thread(exchange_id):
-    thread = threading.Thread(target=simulate_market, args=(exchange_id,))
+def start_simulation_thread(exchange_id, timeout):
+    thread = threading.Thread(target=simulate_market, args=(exchange_id, timeout,))
     thread.start()
     return thread
 
--- stock_market_sim/test_server.py
+++ stock_market_sim/test_server.py
@@ -11,157 +11,127 @@ def client():
 
 @pytest.fixture(scope="session", autouse=True)
 def start_simulations():
-    exchange_ids = [str(uuid.uuid4()) for _ in range(2)]
-    threads = [start_simulation_thread(exchange_id) for exchange_id in exchange_ids]
-    sleep(1)  # Give the threads some time to start
+    exchange_ids = []
     yield exchange_ids
-    for thread in threads:
-        thread.join(0)
 
 def test_start_server(client, start_simulations):
-    for exchange_id in start_simulations:
-        response = client.post(f'/{exchange_id}/start-server', json={
-            'stocks': {'AAPL': {'price': 150}, 'GOOG': {'price': 2800}},
-            'users': {'user1': {'assets': {'AAPL': 10}, 'money': 10000}},
-            'stock_std': 1.0,
-            'headline_min_impact': 1.1,
-            'headline_max_impact': 2.0,
-            'news_headlines': []
-        })
+    response = client.post('/start-server', json={
+        'stocks': ['AAPL', 'GOOG'],
+        'duration': 1,
+        'difficulty': 1
+    })
 
-        assert response.status_code == 200
-        assert response.json['message'] == f"Configuration updated and market simulation started for exchange {exchange_id}."
+    assert response.status_code == 200
+    exchange_id = response.json['exchange_id']
+    start_simulations.append(exchange_id)
+    assert response.json['message'].startswith("Configuration updated and market simulation started for exchange")
+
+    response = client.post('/start-server', json={
+        'stocks': ['AAPL', 'GOOG'],
+        'duration': 1,
+        'difficulty': 1
+    })
+
+    assert response.status_code == 200
+    exchange_id = response.json['exchange_id']
+    start_simulations.append(exchange_id)
+    assert response.json['message'].startswith("Configuration updated and market simulation started for exchange")
 
 def test_get_market_data(client, start_simulations):
     for exchange_id in start_simulations:
         response = client.get(f'/{exchange_id}/market-data')
 
         assert response.status_code == 200
-        assert 'AAPL' in response.json
-        assert 'GOOG' in response.json
+        assert 'prices' in response.json
+        assert 'details' in response.json
 
 def test_place_order_buy(client, start_simulations):
     for exchange_id in start_simulations:
-        # reset the user's assets and money
-        client.post(f'/{exchange_id}/start-server', json={
-            'stocks': {'AAPL': {'price': 150}, 'GOOG': {'price': 2800}},
-            'users': {'user1': {'assets': {'AAPL': 10}, 'money': 10000}},
-            'stock_std': 1.0,
-            'headline_min_impact': 1.1,
-            'headline_max_impact': 2.0,
-            'news_headlines': []
-        })
+        connect_response = client.post(f'/{exchange_id}/connect', json={'name': 'user1'})
+        user_id = connect_response.json['userId']
 
-        response = client.post(f'/{exchange_id}/order/user1', json={
+        response = client.post(f'/{exchange_id}/order', json={
+            'userId': user_id,
             'stock': 'AAPL',
             'quantity': 5,
             'type': 'buy'
         })
 
         assert response.status_code == 200
-        
-        user_data = response.json
-        
-        assert user_data['assets']['AAPL'] == 15
-        assert user_data['money'] < 10000
+        assert response.json['message'].startswith("Order executed")
 
 def test_place_order_sell(client, start_simulations):
     for exchange_id in start_simulations:
-        # reset the user's assets and money
-        client.post(f'/{exchange_id}/start-server', json={
-            'stocks': {'AAPL': {'price': 150}, 'GOOG': {'price': 2800}},
-            'users': {'user1': {'assets': {'AAPL': 10}, 'money': 10000}},
-            'stock_std': 1.0,
-            'headline_min_impact': 1.1,
-            'headline_max_impact': 2.0,
-            'news_headlines': []
-        })
+        connect_response = client.post(f'/{exchange_id}/connect', json={'name': 'user1'})
+        user_id = connect_response.json['userId']
 
-        client.get(f'/{exchange_id}/market-data')
-        response = client.post(f'/{exchange_id}/order/user1', json={
+        response = client.post(f'/{exchange_id}/order', json={
+            'userId': user_id,
             'stock': 'AAPL',
             'quantity': 5,
             'type': 'sell'
         })
 
         assert response.status_code == 200
-        
-        user_data = response.json
-        
-        assert user_data['assets']['AAPL'] == 5
-        assert user_data['money'] > 10000
+        assert response.json['message'].startswith("Order executed")
 
 def test_add_news(client, start_simulations):
     for exchange_id in start_simulations:
         response = client.post(f'/{exchange_id}/add-news', json={
-            'headline': {
-                'stock': 'AAPL',
-                'sentiment': 'up'
-            }
+            'stock': 'AAPL',
+            'impact': 'up'
         })
 
         assert response.status_code == 200
-        assert response.json['message'] == f"News headline published for exchange {exchange_id}. Market will be affected."
+        assert response.json['message'].startswith("News headline published for exchange")
 
 def test_market_simulation(client, start_simulations):
     for exchange_id in start_simulations:
-        initial_prices = {stock: data['price'] for stock, data in exchanges[exchange_id]['config']['stocks'].items()}
+        initial_prices = client.get(f'/{exchange_id}/market-data').json['prices']
         
         sleep(5)
         
-        response = client.get(f'/{exchange_id}/market-data')
-        assert response.status_code == 200
-        
-        current_prices = response.json
+        current_prices = client.get(f'/{exchange_id}/market-data').json['prices']
         for stock in initial_prices:
-            assert initial_prices[stock] != current_prices[stock]['price']
+            assert initial_prices[stock] != current_prices[stock]
 
 def test_isolation_between_exchanges(client, start_simulations):
-    exchange_id_1, exchange_id_2 = start_simulations
 
-    # buy order exchange 1
-    client.post(f'/{exchange_id_1}/start-server', json={
-        'stocks': {'AAPL': {'price': 150}, 'GOOG': {'price': 2800}},
-        'users': {'user1': {'assets': {'AAPL': 10}, 'money': 10000}},
-        'stock_std': 1.0,
-        'headline_min_impact': 1.1,
-        'headline_max_impact': 2.0,
-        'news_headlines': []
-    })
-    client_1 = client.post(f'/{exchange_id_1}/order/user1', json={
+    exchange_id_1, exchange_id_2 = start_simulations[:2]
+
+    connect_response_1 = client.post(f'/{exchange_id_1}/connect', json={'name': 'user1'})
+    assert connect_response_1.status_code == 200
+    user_id_1 = connect_response_1.json['userId']
+    connect_response_2 = client.post(f'/{exchange_id_2}/connect', json={'name': 'user2'})
+    assert connect_response_2.status_code == 200
+    user_id_2 = connect_response_2.json['userId']
+
+    client.post(f'/{exchange_id_1}/order', json={
+        'userId': user_id_1,
         'stock': 'AAPL',
         'quantity': 5,
         'type': 'buy'
-    }).json
-
-    # sell order exchange 2
-    client.post(f'/{exchange_id_2}/start-server', json={
-        'stocks': {'AAPL': {'price': 150}, 'GOOG': {'price': 2800}},
-        'users': {'user2': {'assets': {'AAPL': 10}, 'money': 10000}},
-        'stock_std': 1.0,
-        'headline_min_impact': 1.1,
-        'headline_max_impact': 2.0,
-        'news_headlines': []
     })
-    client_2 = client.post(f'/{exchange_id_2}/order/user2', json={
+
+    client.post(f'/{exchange_id_2}/order', json={
+        'userId': user_id_2,
         'stock': 'AAPL',
         'quantity': 5,
         'type': 'sell'
-    }).json
+    })
 
-    sleep(15)
+    sleep(5)
+
     response_1 = client.get(f'/{exchange_id_1}/market-data')
     response_2 = client.get(f'/{exchange_id_2}/market-data')
-    
+
     assert response_1.status_code == 200
     assert response_2.status_code == 200
 
-    prices_1 = response_1.json
-    prices_2 = response_2.json
+    prices_1 = response_1.json['prices']
+    prices_2 = response_2.json['prices']
 
-    assert client_1['assets']['AAPL'] == 15
-    assert client_2['assets']['AAPL'] == 5
-    assert prices_1['AAPL']['price'] != prices_2['AAPL']['price']
+    assert prices_1['AAPL'] != prices_2['AAPL']
 
 if __name__ == "__main__":
     pytest.main()
